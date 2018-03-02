0551b9d045f7694f6435445070a008e8e1dc4e72
Julia Lidstrom <juleslidstrom@gmail.com>
2018-03-02 08:18:56 -0600

    Fully working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friendsArray = [
     {
-    name:"Amanda Graff",
-    photo:"https://cdn.pixabay.com/photo/2017/01/07/20/38/portrait-1961529_960_720.jpg",
-    scores: [5,2,5,3,4,2,1,2,5,3],
-    comparisonScore:0
+        name:"Amanda Graff",
+        photo:"https://www.stockvault.net/data/2009/01/04/107204/preview16.jpg",
+        scores: [5,2,5,3,4,2,1,2,5,3],
+        comparisonScore:0
     },
     {
         name:"John Jackson",
@@ -13,7 +13,7 @@ var friendsArray = [
     },
     {
         name:"Natalie Steele",
-        photo:"https://cdn.pixabay.com/photo/2018/02/27/17/14/cap-3186008_960_720.jpg",
+        photo:"https://www.stockvault.net/data/2007/11/06/103798/preview16.jpg",
         scores: [2,2,1,5,4,2,1,3,1,3],
         comparisonScore:0
     },
@@ -25,14 +25,14 @@ var friendsArray = [
     },
     {
         name:"Shannon Smith",
-        photo:"https://cdn.pixabay.com/photo/2018/02/25/23/12/woman-3182019_960_720.jpg",
+        photo:"https://www.stockvault.net/data/2017/12/02/240423/preview16.jpg",
         scores: [5,1,1,3,4,1,4,2,1,3],
         comparisonScore:0
     },
     {
         name:"Elizabeth Hanley",
-        photo:"https://cdn.pixabay.com/photo/2016/04/10/13/52/portrait-1319951_960_720.jpg",
-        scores: [5,1,1,3,4,1,4,2,1,3],
+        photo:"https://www.stockvault.net/data/2017/05/23/236418/preview16.jpg",
+        scores: [3,2,1,5,1,1,3,5,5,4],
         comparisonScore:0
     }
 ];
