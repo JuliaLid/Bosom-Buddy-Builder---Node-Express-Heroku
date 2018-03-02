38184f4fa6fbe51dec9f8bc1e0f3bd2bf322ed87
Julia Lidstrom <juleslidstrom@gmail.com>
2018-03-02 12:11:28 -0600

    Working app minus an image

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+// app.use('/images', express["static"](path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
