62564cf9138d9d6a77f5478aaa66ad742793c925
Julia Lidstrom <juleslidstrom@gmail.com>
2018-03-02 12:37:27 -0600

    Working image

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname,'./app/public')));
 // app.use('/images', express["static"](path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
